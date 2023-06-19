d803eeceff797a1c8f759da55de126adea947f02
Steve Pflug <stephen@pflug.us>
2023-06-18 20:02:45 -0500

    first commit

---
+++ src/main/java/com/example/fullstackkeys/FullstackKeysApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackkeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackKeysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackKeysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullstackkeys/FullstackKeysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackkeys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackKeysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
