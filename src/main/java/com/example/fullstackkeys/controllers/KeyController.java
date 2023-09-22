1f692950ea6e05558ed5f610505b526020083fcc
Steve Pflug <stephen@pflug.us>
2023-09-22 12:45:04 -0500

    completed lecture

--- src/main/java/com/example/fullstackkeys/controllers/KeyController.java
+++ src/main/java/com/example/fullstackkeys/controllers/KeyController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KeyController {
 
-//    @Autowired
-//    private Keys keys;
-//
-//    @GetMapping(value="/keys.js", produces = "application/javascript")
-//    public String getKeys() {
-//        return String.format("""
-//                const TEST_KEY = "%s";
-//                const SECOND_KEY = "%s";
-//        """, keys.getTEST_KEY(), keys.getSECOND_KEY());
-//    }
+    @Autowired
+    private Keys keys;
+
+    @GetMapping(value="/keys.js", produces = "application/javascript")
+    public String getKeys() {
+        return String.format("""
+                const TEST_KEY = "%s";
+                const SECOND_KEY = "%s";
+        """, keys.getTEST_KEY(), keys.getSECOND_KEY());
+    }
 }
--- src/main/java/com/example/fullstackkeys/services/Keys.java
+++ src/main/java/com/example/fullstackkeys/services/Keys.java
@@ -6,24 +6,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class Keys {
 
-//    @Value("${test.key}")
-//    private String TEST_KEY;
-//    @Value("${second.key}")
-//    private String SECOND_KEY;
-//
-//    public String getTEST_KEY() {
-//        return TEST_KEY;
-//    }
-//
-//    public void setTEST_KEY(String TEST_KEY) {
-//        this.TEST_KEY = TEST_KEY;
-//    }
-//
-//    public String getSECOND_KEY() {
-//        return SECOND_KEY;
-//    }
-//
-//    public void setSECOND_KEY(String SECOND_KEY) {
-//        this.SECOND_KEY = SECOND_KEY;
-//    }
+    @Value("${test.key}")
+    private String TEST_KEY;
+    @Value("${second.key}")
+    private String SECOND_KEY;
+
+    public String getTEST_KEY() {
+        return TEST_KEY;
+    }
+
+    public void setTEST_KEY(String TEST_KEY) {
+        this.TEST_KEY = TEST_KEY;
+    }
+
+    public String getSECOND_KEY() {
+        return SECOND_KEY;
+    }
+
+    public void setSECOND_KEY(String SECOND_KEY) {
+        this.SECOND_KEY = SECOND_KEY;
+    }
 }
